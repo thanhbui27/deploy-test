3e4846d18df453bc9e786eea61e81ae87f99c55d
thanhbui27 <bthanh2001@gmail.com>
2025-04-07 22:25:32 +0700

    2

--- adonisrc.ts
+++ adonisrc.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from '@adonisjs/core/app'
 
 export default defineConfig({
-  typescript: true,
   /*
   |--------------------------------------------------------------------------
   | Commands
